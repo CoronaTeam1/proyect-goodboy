050c2cf466539dfa031b75017896b4b2719711d3
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-03-28 14:27:15 +0100

    carousel

--- client/src/components/elements/Carousel/Carousel.jsx
+++ client/src/components/elements/Carousel/Carousel.jsx
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { Grid } from "@material-ui/core";
 import Slice from "./Slice";
-import style from "./style";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux"
 
 
 const Carousel = () => {
 
-  const [data, useData] = useState([
-    { url: "../../../../images/index-dog1.jpeg", text: "lorenanandgsdgsdgdsjg dgsdgsidg sdig sdg sdgs" },
-    { url: "../../../../images/index-dog2.jpg", text: "faefaedjosngsjdngdjsngdm sd gs dgsdgdsgfns" },
-    { url: "../../../../images/index-dog3.jpg", text: "loafe sdgsdgsdg sdgsdgsdgdsgdsgds gsdgdsfafenans" }
-  ]);
+  const data= [
+    { url: "../../../../images/index-dog1.jpeg", text: "lorenanandgsdgsdgdsjg dgsdgsidg sdig sdg sdgs", number: 1 },
+    { url: "../../../../images/index-dog2.jpg", text: "faefaedjosngsjdngdjsngdm sd gs dgsdgdsgfns", number: 2 },
+    { url: "../../../../images/index-dog3.jpg", text: "loafe sdgsdgsdg sdgsdgsdgdsgdsgds gsdgdsfafenans", number: 3 }
+  ]
 
   const dispatch = useDispatch()
 
@@ -20,7 +19,7 @@ const Carousel = () => {
     <Grid container row justify="space-between"
       alignItems="center" className="index-carousel">
       <button onClick={() => dispatch({type: "PREV"})}>anterior</button>
-      <Slice {...data[useSelector(state => state.slice)]}/>
+      <Slice {...data[useSelector(state => state.slice)]} />
       <button onClick={() => dispatch({type: "NEXT"})}>siguiente</button>
     </Grid>
   );
--- client/src/components/elements/Carousel/Slice.jsx
+++ client/src/components/elements/Carousel/Slice.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
-import {Grid} from '@material-ui/core'
+import { Grid } from "@material-ui/core";
 import style from "./style";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
 
+const Slice = ({ url, text, number }) => {
+  const styleClass = style();
 
-const Slice = ({url, text}) => {
-     const styleClass = style()
-
-    return (<Grid item xs={4}>
-            <div className={styleClass.containerImage}>
-                <img src={url} alt="dog index"  className={styleClass.image}/>
-            </div>
-            <p>{text}</p>
-            </Grid>
-    )
+  return (
+    <SwitchTransition mode="out-in">
+      <CSSTransition
+        transitionName="carousel"
+        transitionEnterTimeout={300}
+        transitionLeaveTimeout={300}
+      >
+        <Grid item xs={4} className="slice">
+          <div className={styleClass.containerImage}>
+            <img src={url} alt="dog index" className={styleClass.image} />
+          </div>
+          <p>{text}</p>
+        </Grid>
+      </CSSTransition>
+    </SwitchTransition>
+  );
 };
 export default Slice;
