c61108c64e322aaad96bb8da950f0e83a7f800a1
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-04-01 11:59:49 +0200

    finish carousel

--- client/src/components/elements/OnBoarding/Carousel/Slice.jsx
+++ client/src/components/elements/OnBoarding/Carousel/Slice.jsx
@@ -4,16 +4,18 @@ import style from "./Style";
 import { SwitchTransition, CSSTransition } from "react-transition-group";
 
 const Slice = ({ url, text, hidden }) => {
-const styleClass = style();
+  const styleClass = style();
   return (
-      <CSSTransition in={hidden} timeout={{ enter: 5000, exit: 5000 }} classNames="my-node">
-        <Grid xs={4} className="slice center">
-          <div className={styleClass.containerImage}>
-            <img src={url} alt="dog index" className={styleClass.image} />
-          </div>
-          <p className={styleClass.p}>{text}</p>
-        </Grid>
-      </CSSTransition>
+    <CSSTransition
+      in={hidden}
+      timeout={{ enter: 5000, exit: 5000 }}
+      classNames="my-node"
+    >
+      <Grid xs={4} className="slice center">
+        <img src={url} alt="dog index" className={styleClass.image} />
+        <p className={styleClass.p}>{text}</p>
+      </Grid>
+    </CSSTransition>
   );
 };
 export default Slice;
