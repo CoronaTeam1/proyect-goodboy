04c0d28fbb9706cae8deeb480c0e906eae68b5bc
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-04-09 16:17:07 +0200

    change route

--- server/routes/breed.routes.js
+++ server/routes/breed.routes.js
@@ -4,32 +4,20 @@ const cheerio = require('cheerio')
 const axios = require('axios').default
 
 router.post('/', (req, res, next) => {
-   const index = req.body.index
-   console.log(index)
-   index === 0 ? axios.create().get('https://www.purina.es/perros/razas-de-perro/tipos-de-razas-de-perro')
-       .then(response => {
-           const $ = cheerio.load(response.data);
-           let arrNames = []
-           $('.result-animal-container').each((i, e) => {
-               const div = $(e).children()
-               const name = $(div).find('.results-view-name').text()
-               arrNames.push(name)
-           })
-           return arrNames
-       }).then(response => res.json(response))
-           .catch(err => console.log(err)) : axios.create().get(`https://www.purina.es/perros/razas-de-perro/tipos-de-razas-de-perro?breed_name=&page=${index}`)
-           .then(response => {
-               const $ = cheerio.load(response.data);
-                arrNames = []
-               $('.result-animal-container').each((i, e) => {
-                   const div = $(e).children()
-                   const name = $(div).find('.results-view-name').text()
-                   arrNames.push(name)
-               })
-               return arrNames
-           })
-       .then(response => res.json(response))
-       .catch(err => console.log(err));
+    const index = req.body.index
+    axios.create().get(`https://www.purina.es/perros/razas-de-perro/tipos-de-razas-de-perro?breed_name=&page=${index}`)
+        .then(response => {
+            const $ = cheerio.load(response.data);
+            arrNames = []
+            $('.result-animal-container').each((i, e) => {
+                const div = $(e).children()
+                const name = $(div).find('.results-view-name').text()
+                arrNames.push(name)
+            })
+            return arrNames
+        })
+        .then(response => res.json(response))
+        .catch(err => console.log(err));
 })
 
 
