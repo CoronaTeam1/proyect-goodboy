d6cbbc154537e901aea5f1e62f12165dcc1edb2a
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-04-30 14:01:05 +0200

    training

--- client/src/components/elements/Home/Sections/Learn/LearnDetails.jsx
+++ client/src/components/elements/Home/Sections/Learn/LearnDetails.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
+import Header from "../../../../ui/Header/Header";
+import Footer from "../../../../ui/Footer/Footer";
+import DifficultyBar from "../../../../ui/Bars/DifficultyBar";
+import InstrucctionsBar from "../../../../ui/Bars/InstrucctionsBar";
+import TrainingServices from "../../../../../services/training.services";
+
+
+
 
 const LearnDetails = () => {
-    const { learn } = useParams();
 
-    return (
-        <>
-        <h1>{learn}</h1>
-        </>
-    )
-}
+  const [training, setTraining] = useState()
+  const { learn } = useParams();
 
-export default LearnDetails
+  const getTraining = () => {
+    TrainingServices.getTraining(learn)
+      .then(elm => console.log(elm))
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    getTraining();
+  }, []);
+
+  return (
+    <>
+      <Header title={learn} />
+          <iframe width="100%" height="200px" src="https://www.youtube.com/embed/wjBg5Nlx1HM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+     <DifficultyBar />
+    <div className="overflow">
+      <p>Es es esencial enseñarle a pasear con correa. El adiestramiento debe comenzar en cuanto el perro llegue a casa, no importa qué edad tenga. De hecho, en algunos países el uso de correas es obligatorio por ley.</p>
+      <InstrucctionsBar />
+      <InstrucctionsBar />
+      <InstrucctionsBar />
+    </div>
+      <Footer />
+    </>
+  );
+};
+
+export default LearnDetails;
---
+++ client/src/components/ui/Bars/DifficultyBarStyle.jsx
@@ -0,0 +1,32 @@
+import { makeStyles } from "@material-ui/styles";
+import { findByLabelText } from "@testing-library/react";
+
+export default makeStyles({
+  topHeading: {
+    display: "flex",
+    justifyContent: "space-around",
+    backgroundColor: "#6AC3AE",
+    color: "#FFFFFF",
+    textAlign: "center",
+    padding: "12px 0",
+    fontSize: "30px",
+  },
+  list: {
+    padding: "10px 40px",
+    background: "#FFFFFF",
+    boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.09)",
+    borderRadius: "4px",
+    fontFamily: "Helvetica",
+    fontSize: "16px",
+    color: "#6AC3AE",
+    border: "1px solid #6AC3AE",
+  },
+  center:{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+  },
+text: {
+    margin: 0,
+}
+});
